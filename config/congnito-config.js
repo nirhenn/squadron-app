b7e5944cf38996567ee9992380277e785ead846e
Nir Henn <nirhenn@users.noreply.github.com>
2020-09-24 18:44:28 +0300

    Update congnito-config.js

--- config/congnito-config.js
+++ config/congnito-config.js
@@ -1,13 +1,13 @@
 
 let config = {
-    "AWS_ACCOUNT_ID": "0812418617227",
+    "AWS_ACCOUNT_ID": "xxxxxxxxxxx",
     "AWS_REGION": "eu-central-1",
-    "COGNITO_IDENTITY_POOL_ID": "eu-central-1_WbNkNZKDt",
-    "IAM_ROLE_ARN": "arn:aws:cognito-idp:eu-central-1:812418617227:userpool/eu-central-1_WbNkNZKDt",
-    "COGNITO_DATASET_NAME": "squadron",
-    "COGNITO_KEY_NAME": "online",
+    "COGNITO_IDENTITY_POOL_ID": "eu-central-1_xxxxxxxxx",
+    "IAM_ROLE_ARN": "arn:aws:cognito-idp:eu-central-1:xxxxxxxxxxxxx:userpool/eu-central-1_xxxxxxxxxx",
+    "COGNITO_DATASET_NAME": "xxxxxxxxxxxx",
+    "COGNITO_KEY_NAME": "xxxxxxx",
     "CALLBACKURL": "https://squadron.auth.eu-central-1.amazoncognito.com/callback",
-    "AMAZON_CLIENT_ID": "7f9b1bi7cik7svnqvb5qfhcerr", 
+    "AMAZON_CLIENT_ID": "xxxxxxxxxxxxxxxxxxxxxx", 
     "AMAZON_CLIENT_SECRET": "" 
 };
 
