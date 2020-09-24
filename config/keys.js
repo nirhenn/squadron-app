b93700dfd402214fb790907bfdd3b812e031c012
Nir Henn <nirhenn@users.noreply.github.com>
2020-09-24 18:45:09 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://Nchen:Nchen2371@cognitouserpool.izeqp.mongodb.net/CognitoUserPool?retryWrites=true&w=majority",
-  secretOrKey: "secret"
+  mongoURI: "mongodb+srv://xxxxxxx:xxxxxxxxxxx@cognitouserpool.izeqp.mongodb.net/CognitoUserPool?retryWrites=true&w=majority",
+  secretOrKey: "xxxxxxxxxxxx"
 };
